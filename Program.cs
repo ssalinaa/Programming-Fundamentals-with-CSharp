5f3aca42ac170e0fe20dab4ae04e3b398ffb573f
ssalinaa <112023119+ssalinaa@users.noreply.github.com>
2022-10-13 23:14:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Car
+{
+    class Vehicle
+    {
+        public string brand = "Ford";
+        public void punk()
+        {
+            Console.WriteLine("Tuuuut!");
+        }
+    }
+    class Car : Vehicle
+    {
+        public string model = "Mustang";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.punk();
+            Console.WriteLine(myCar.brand + " " + myCar.model);
+
+        }
+    }
+}
